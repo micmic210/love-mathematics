2d158e5ea5082fa2892e6d324193ba9ff9642543
Michiko  Inoue <inouem888@gmail.com>
2024-05-24 15:06:50 +0000

    Set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivisionQuestion() {
+
+}
+
